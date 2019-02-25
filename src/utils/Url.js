aa6768b35bf4955e2f37721203903793ff95b43f
sanddhy <sandy.mulato.r@gmail.com>
2019-02-24 20:48:22 -0600

    api url heroku

--- src/utils/Url.js
+++ src/utils/Url.js
@@ -1,2 +1,4 @@
-const base_url = "http://localhost:3000/";
-export const api_url = base_url + "api/v1/"
+// const base_url = "http://localhost:3000/";
+// heroku  api url
+const base_url = 'https://infinite-taiga-21607.herokuapp.com/api/v1/propertys'
+export const api_url = base_url + 'api/v1/'
