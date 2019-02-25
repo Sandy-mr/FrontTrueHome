6ebef612c0be65994d3c94578201fd44700a7142
Sandy-mr <sandy.mulato.r@gmail.com>
2019-02-24 20:57:05 -0600

    fix url api

--- src/utils/Url.js
+++ src/utils/Url.js
@@ -1,4 +1,4 @@
 // const base_url = "http://localhost:3000/";
 // heroku  api url
-const base_url = 'https://infinite-taiga-21607.herokuapp.com/api/v1/propertys'
+const base_url = 'https://infinite-taiga-21607.herokuapp.com/'
 export const api_url = base_url + 'api/v1/'
